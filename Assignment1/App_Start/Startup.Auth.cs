d1921261c73d9f9bf74ae36f0ea64bc61bf8c00c
Oleg Pozovnoy <pozovnoy.oleg@gmail.com>
2019-03-19 09:39:41 +0300

    attempt to resolve db connection problem1

--- Assignment1/App_Start/Startup.Auth.cs
+++ Assignment1/App_Start/Startup.Auth.cs
@@ -47,9 +47,9 @@ namespace Assignment1
             app.UseTwoFactorRememberBrowserCookie(DefaultAuthenticationTypes.TwoFactorRememberBrowserCookie);
 
             // Uncomment the following lines to enable logging in with third party login providers
-            app.UseMicrosoftAccountAuthentication(
-                clientId: ConfigurationManager.AppSettings["MicrosoftClientId"],
-                clientSecret: ConfigurationManager.AppSettings["MicrosoftClientSecret"]);
+            //app.UseMicrosoftAccountAuthentication(
+            //    clientId: ConfigurationManager.AppSettings["MicrosoftClientId"],
+            //    clientSecret: ConfigurationManager.AppSettings["MicrosoftClientSecret"]);
 
             //app.UseTwitterAuthentication(
             //   consumerKey: "",
