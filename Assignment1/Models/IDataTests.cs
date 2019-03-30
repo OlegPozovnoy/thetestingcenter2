79c6566cc786cc54a441af098463c2e2783e6c7c
Oleg Pozovnoy <pozovnoy.oleg@gmail.com>
2019-03-30 07:18:58 +0300

    IDataTests and IMockTests were added

---
+++ Assignment1/Models/IDataTests.cs
@@ -0,0 +1,39 @@
+﻿using System.Linq;
+
+namespace Assignment1.Models
+{
+    public class IDataTests : IMockTests
+    {
+        // db connection
+        private DbModel db = new DbModel();
+
+        public IQueryable<Test> Categories { get { return db.Tests; } }
+
+        public void Delete(Test test)
+        {
+            db.Tests.Remove(test);
+            db.SaveChanges();
+        }
+
+        public Test Save(Test test)
+        {
+            if (test.Id == 0)
+            {
+                db.Tests.Add(test);
+            }
+            else
+            {
+                db.Entry(test).State = System.Data.Entity.EntityState.Modified;
+            }
+
+            db.SaveChanges();
+            return test;
+        }
+
+        public void Dispose()
+        {
+            db.Dispose();
+        }
+
+    }
+}
---
+++ Assignment1/Models/IMockTests.cs
@@ -0,0 +1,12 @@
+﻿using System.Linq;
+
+namespace Assignment1.Models
+{
+    public interface IMockTests
+    {
+        IQueryable<Test> Categories { get; }
+        Test Save(Test category);
+        void Delete(Test category);
+        void Dispose();
+    }
+}
